703beaeb14bdb529cd1f77ae5e5c5e42db96fa33
congduc512001 <congduc512001@gmail.com>
2024-07-07 20:30:16 +0700

    commit7/7

--- js/main.js
+++ js/main.js
@@ -3,14 +3,6 @@ $(document).ready(function () {
    jQuery(window).scroll(function() {
        header();
    });
-    // $(document).on("click", ".header_menu", function () {
-    //    $(".menu").addClass("open");
-    //    $('body').addClass('none_scroll');
-    // });
-    // $(document).on("click", ".close", function () {
-    //     $(".menu").removeClass("open");
-    //     $('body').removeClass('none_scroll');
-    //  });
     $(".hamburger").click(function () {
         $(this).toggleClass("is-active");
     });
